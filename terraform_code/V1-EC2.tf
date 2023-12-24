506cdbd4dab0b2f7a0a4e70f09e8a776f79f90f6
Krishna Kishore <vmandala@netapp.com>
2023-12-24 19:51:13 +0530

    new origin

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -6,30 +6,5 @@ resource "aws_instance" "demo-server" {
   ami = "ami-0a0f1259dd1c90938" 
   instance_type = "t2.micro"
   key_name = "DPP"
-  security_groups = [ "demo-sg" ]
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH access"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "SSH Access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SSH-PORT"
-  }
-}
